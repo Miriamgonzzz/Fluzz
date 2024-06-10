52d0d5143818368cce08bed89675cb9f22e14b25
Miriam Gonzalez <mgonzalezb11@alumnos.nebrija.es>
2024-06-11 00:06:25 +0200

    Punto de parada de camara cuando el boss

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float speed = 2f; // Velocidad de movimiento de la cámara
+    public Transform puntoDeParada; // El punto donde queremos que la cámara se detenga
+    public float velocidadCamara = 2f; // La velocidad de la cámara
+
+    private bool detenerCamara = false; // Indica si la cámara debe detenerse
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +20,18 @@ public class CameraMovement : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+        // Si no se ha detenido la cámara y el punto de parada está asignado
+        if (!detenerCamara && puntoDeParada != null)
+        {
+            // Movemos la cámara hacia el punto de parada a una velocidad constante
+            transform.position = Vector3.MoveTowards(transform.position, puntoDeParada.position, velocidadCamara * Time.deltaTime);
+
+            // Si la cámara llega al punto de parada, detenemos su movimiento
+            if (transform.position == puntoDeParada.position)
+            {
+                detenerCamara = true;
+                Debug.Log("La cámara ha llegado al punto de parada y se ha detenido.");
+            }
+        }
     }
 }
