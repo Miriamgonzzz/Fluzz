821498d5fe61364de5d03e09deb5e9a0091bd8f4
Miriam Gonzalez <mgonzalezb11@alumnos.nebrija.es>
2024-06-13 22:55:36 +0200

    Ataque fenix y colaisders enemigos

--- Assets/Scripts/Cloudy.cs
+++ Assets/Scripts/Cloudy.cs
@@ -6,8 +6,17 @@ public class Cloudy : MonoBehaviour
 {
     public float speed = 5f;
     // Start is called before the first frame update
+    [SerializeField] public int vida = 10;
+    private BoxCollider2D colliderEnemigo;
+    public Animator animator;
+    public Color colorReposo;
+    public Color colorDano;
+    SpriteRenderer spriteRenderer;
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        colliderEnemigo = GetComponent<BoxCollider2D>();
 
     }
 
@@ -25,5 +34,61 @@ public class Cloudy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            vida -= 10;
+
+            if (vida <= 0)
+            {
+
+                colliderEnemigo.enabled = false;
+                animator.SetBool("muerte", true);
+
+            }
+            StopAllCoroutines();
+            StartCoroutine(Dano());
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Bola")
+        {
+
+            vida -= 10;
+            transform.localScale = new Vector3(-10, 1, 1);
+            if (vida <= 0)
+            {
+
+                colliderEnemigo.enabled = false;
+                animator.SetBool("muerte", true);
+
+            }
+            StopAllCoroutines();
+            StartCoroutine(Dano());
+        }
+
+
+    }
+
+    private void destruirEnemigo()
+    {
+        Destroy(gameObject);
+    }
+
+
+    IEnumerator Dano()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = colorReposo;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = colorDano;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = colorReposo;
     }
 }
